a7aa4e985a39d49f7ee1bfb963dc2e416943ebca
marcelinaov <anaortiz@Anas-MacBook-Pro.local>
2019-05-06 20:41:06 -0600

    adding packages

---
+++ app/data/friends.js
@@ -0,0 +1,31 @@
+var friends = [
+	{
+		name: "Dr. Strange",
+		photo: "https://en.wikipedia.org/wiki/Doctor_Strange#/media/File:Doctor_Strange_Vol_4_2_Ross_Variant_Textless.jpg",
+		scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+	},
+	{
+		name: "Deadpool",
+		photo: "https://en.wikipedia.org/wiki/Deadpool#/media/File:Deadpool.png",
+		scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+	},
+	{
+		name: "Venom",
+		photo: "https://en.wikipedia.org/wiki/Venom_(Marvel_Comics_character)#/media/File:Web_of_Venom.png",
+		scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+	},
+	{
+		name: "Human Torch",
+		photo: "https://en.wikipedia.org/wiki/Human_Torch#/media/File:Human_Torch_(Johnny_Storm).png",
+		scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+	},
+	{
+		name: "Wolverine",
+		photo: "https://en.wikipedia.org/wiki/Wolverine_(character)#/media/File:Marvelwolverine.jpg",
+		scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+	}
+];
+
+module.exports = friends;
+
+
